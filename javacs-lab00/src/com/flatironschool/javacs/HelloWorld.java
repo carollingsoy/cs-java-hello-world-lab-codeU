56feda6608082d75ff74f819701994f083a773ea
Soyeun Carol Jung <sj2674@columbia.edu>
2016-03-20 03:49:09 -0400

    whee finished first lab

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -1,11 +1,18 @@
 package com.flatironschool.javacs;
 
 public class HelloWorld {
-    public static Double getVersion() {
-        // I won't work!
+    private String version;
+
+    public HelloWorld() {
+	version = System.getProperty("java.specification.version");
+    }
+
+    public double getVersion() {
+        return Double.parseDouble(version);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	HelloWorld main = new HelloWorld();
+	System.out.println(main.getVersion());
     }
 }
